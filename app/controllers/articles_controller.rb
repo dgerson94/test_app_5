af94b7e4e08386216cb36790d1191a3172643b81
dov_gerson <dov.gerson@anyword.com>
2023-06-12 20:53:13 +0300

    creating articles
    Signed-off-by: dov_gerson <dov.gerson@anyword.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,20 @@
 
 class ArticlesController < ApplicationController
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article.save!
+    redirect_to @article
   end
 end
